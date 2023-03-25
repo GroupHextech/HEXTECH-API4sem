b21990322a5b0fd2aa9ba8429bf78f2ec19f0574
Daniloel <zoltancao@gmail.com>
2023-03-24 21:15:55 -0300

    backend API_lV
    Primeiro commit

---
+++ acc-backend/Backend_lV_Fatec_Embraer/Backend_lV_Fatec_Embraer/src/main/java/HexTech/Backend_lV_Fatec_Embraer/BackendLVFatecEmbraerApplication.java
@@ -0,0 +1,13 @@
+package HexTech.Backend_lV_Fatec_Embraer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLVFatecEmbraerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLVFatecEmbraerApplication.class, args);
+	}
+
+}
---
+++ acc-backend/Backend_lV_Fatec_Embraer/Backend_lV_Fatec_Embraer/src/test/java/HexTech/Backend_lV_Fatec_Embraer/BackendLVFatecEmbraerApplicationTests.java
@@ -0,0 +1,13 @@
+package HexTech.Backend_lV_Fatec_Embraer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLVFatecEmbraerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
